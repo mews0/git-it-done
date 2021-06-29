e09b51e781d800d5e786e92bdc20431407e26619
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-06-29 18:00:58 -0500

    organize code for readability

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,33 +1,29 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
-  console.log(repo);
+  // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+  // make a get request to url
   fetch(apiUrl).then(function(response) {
     // request was successful
     if (response.ok) {
       response.json().then(function(data) {
-        // pass response data to dom function
         displayIssues(data);
+
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
       });
     }
     else {
+      console.log(response);
       alert("There was a problem with your request!");
     }
   });
-
-  if (response.ok) {
-    response.json().then(function(data) {
-      displayIssues(data);
-  
-      // check if api has paginated issues
-      if (response.headers.get("Link")) {
-        displayWarning(repo);
-      }
-    });
-  }
 };
 
 var displayIssues = function(issues) {
@@ -63,6 +59,7 @@ var displayIssues = function(issues) {
     // append to container
     issueEl.appendChild(typeEl);
 
+    // append to the dom
     issueContainerEl.appendChild(issueEl);
   }
 };
@@ -71,6 +68,7 @@ var displayWarning = function(repo) {
   // add text to warning container
   limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
+  // create link element
   var linkEl = document.createElement("a");
   linkEl.textContent = "See More Issues on GitHub.com";
   linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
